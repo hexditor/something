82e779cd36cd54e7151bf15eb2e23849d69afa8a
hexditor <1563615279zxy@gmail.com>
2025-09-21 16:18:50 +0800

    更新 课堂笔记.cpp

--- Project2/课堂笔记.cpp
+++ Project2/课堂笔记.cpp
@@ -1,4 +1,4 @@
-//����С���ķ���:
+//保留小数的方法:
 #include <iomanip>
 #include <iostream>
 using namespace std;
@@ -6,8 +6,8 @@ using namespace std;
 {
 
     double k = 0.123456;
-    //���������Ǹ�����
-    //ǰ����������ȫ�֣����һ��ֻ������һ������
+    //变量必须是浮点型
+    //前三种作用于全局且需要头文件，最后一种只作用于一个变量
     cout << setiosflags(ios::fixed) << setprecision(2) << endl;
 
     cout.setf(ios::fixed);
@@ -16,48 +16,49 @@ using namespace std;
     cout << fixed << setprecision(2) << endl;
 
     printf("%.3f", k);
+如果想使用科学计数法，要用printf("%.3e", k);
 }
 
 
-//ָ��:
+//指针:
 /*
-1.ȡַ��:&
+1.取址符:&
 double <<a;
-cout <<&a; //�����a�ĵ�ַ(Ҳ��ָ��)
-2.��ַ��:*
+cout <<&a; //会输出a的地址(也叫指针)
+2.间址符:*
 cout <<*(&a);
-//��ʾ����a�ĵ�ַ��ָ�Ķ��󣬼�����a
-3.����ָ��: int *
-int *a;    //��ʱa������ǵ�ַ
+//表示变量a的地址所指的对象，即变量a
+3.整型指针: int *
+int *a;    //此时a储存的是地址
 int p=55;
-a=&p;      //��ʱa�������p�ĵ�ַ
-cout <<a;  //���p�ĵ�ַ
-cout <<*a; //���"55"
+a=&p;      //此时a储存的是p的地址
+cout <<a;  //输出p的地址
+cout <<*a; //输出"55"
 a++;
-//��ָ������һλ����ʱa����p���һλ���ڴ�ĵ�ַ
-//ͬ��������double *a; ������ָ��
-4.��ָ��
+//将指针右移一位，此时a储存p后侧一位的内存的地址
+//同理，还有double *a; 浮点型指针
+4.空指针
 char *s=0;
 double *p=NULL;
 */
 
-//����: *
+//引用: *
 /*
-�﷨: ���� &������ = ������;
+语法: 类型 &引用名 = 对象名;
 int a;
 int &A=a;
-//��ʱ����a�����������֣�a��A
-���A�������aЧ��һ��
+//此时变量a有了两个名字，a和A
+输出A，与输出a效果一致
 */
 
-//����
+//常量
 /*
-1.  const int p = 1;    //��ֵ
-2.  const int *p;       //��ֵ
-    int const *p;       //��ֵ
-3.  int *const p;       //����ַ
-4.  const int *const p; //��ֵ�͵�ַ
-    int const *const p; //��ֵ�͵�ַ
+1.  const int p = 1;    //锁值
+2.  const int *p;       //锁值
+    int const *p;       //锁值
+3.  int *const p;       //锁地址
+4.  const int *const p; //锁值和地址
+    int const *const p; //锁值和地址
 
 */
 
